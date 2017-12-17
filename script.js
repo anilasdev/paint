da7625a6c187559b90108a84657d648ce1536073
Rakshith Bellare <rakshith_bellare@outlook.com>
2017-12-17 15:00:03 +0530

    Start of the triangle class

--- script.js
+++ script.js
@@ -9,20 +9,67 @@ canvas.height = window.innerHeight;
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-// Algorithm for triangle generation
-function drawTriangle(inEvent) {
-	ctx.beginPath();
-	ctx.moveTo(100, 100);
-	ctx.lineTo(100,300);
-	ctx.lineTo(300,300);
-	ctx.closePath();
-
-	ctx.lineWidth = 10;
-	ctx.strokeStyle = '#666666';
-	ctx.stroke();
-
-	ctx.fillStyle = "#FFCC00";
-	ctx.fill();
+function Triangle(inInitPosX, inInitPosY, inPosX, inPosY, inTriSide, inDistance, inColor) {
+    this.x1 = inInitPosX;
+    this.y1 = inInitPosY;
+    this.x2 = inPosX;
+    this.y2 = inPosY;
+    this.triSide = inTriSide;
+    this.distance = inDistance;
+    this.color = inColor;
 }
 
-canvas.addEventListener("mousedown", drawTriangle, false);
+Triangle.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.moveTo(this.x1, this.y1);
+    ctx.lineTo(this.x2, this.y2);
+    ctx.lineTo(this.x1 + this.triSide/2, this.y1 + this.distance);
+    if(flag == 0) {
+        ctx.closePath();
+    }
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
+
+// Algorithm for triangle generation
+function drawTriangle(inEvent) {  
+    if(flag==1) {
+        // to get the distance between a point where mouse is clicked and a point where mouse drag ends  
+        dist = Math.pow(Math.abs(parseInt(Math.pow(parseInt(inEvent.clientX-initX),2)-Math.pow(parseInt(inEvent.clientY-initY),2))),0.5);
+
+        //To draw equilateral triangle, we have initial point and let us asssume the obtained distance as the hegiht of tringle.
+        //Using this, we can get the side of the tringle, thereby we obtain the right and left side vertices points.
+        triSide=1.1547*dist;
+        var triangleColor = getColor();
+        var triangle = new Triangle(initX, initY, inEvent.clientX, inEvent.clientY, triSide, dist, triangleColor);     
+        triangles.push(triangle);
+        triangle.draw();
+    }
+}
+
+function getColor()
+{
+	return '#'+Math.random().toString(16).slice(-6);
+}
+
+    var flag = 0;
+    var initX,initY;
+    var dist;
+    var triSide;
+    var triangles = [];
+
+// To get the initial point 
+document.addEventListener("mousedown", function(event){
+    flag = 0;
+    initX=event.clientX;
+    initY=event.clientY;
+}, false);
+
+// To enable click into drag event 
+document.addEventListener("mousemove", function(){
+    flag = 1;
+}, false);
+
+// To draw triandle according to drag size
+document.addEventListener("mouseup", drawTriangle, false);
