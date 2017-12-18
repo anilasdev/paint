cc8184b5f8bcc8971a36d595a581c3d786b9bf65
Anil Kumar <anirai.kumar@gmail.com>
2017-12-18 22:19:39 +0530

    Checking whether mouse pointer inside triangle or not

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function Triangle(inInitPosX, inInitPosY, inPosX, inPosY, inTriSide, inDistance,
     this.y1 = inInitPosY;
     this.x2 = inPosX;
     this.y2 = inPosY;
+    this.x3 = inInitPosX + inTriSide/2;
+    this.y3 = inInitPosY + inDistance;
     this.triSide = inTriSide;
     this.distance = inDistance;
     this.color = inColor;
@@ -32,6 +34,18 @@ Triangle.prototype.draw = function() {
 }
 
 
+Triangle.prototype.update = function() {
+    ctx.beginPath();
+    ctx.moveTo(this.x1, this.y1);
+    ctx.lineTo(this.x2, this.y2);
+    ctx.lineTo(this.x1 + this.triSide/2, this.y1 + this.distance);
+    if(flag == 0) {
+        ctx.closePath();
+    }
+    ctx.fillStyle = this.color;
+    ctx.fill();
+}
+
 // Algorithm for triangle generation
 
 function drawTriangle(inEvent) {  
@@ -45,38 +59,25 @@ function drawTriangle(inEvent) {
         var triangleColor = getColor();
         var triangle = new Triangle(initX, initY, inEvent.clientX, inEvent.clientY, triSide, dist, triangleColor);     
         triangles.push(triangle);
+console.log(triangles);
         triangle.draw();
-    }
+    }}
 
-function drawTriangle(inEvent) {
-       // to get the distance between a point where mouse is clicked and a point where mouse drag ends  
-     dist=Math.pow(Math.abs(parseInt(Math.pow(parseInt(inEvent.clientX-initX),2)-Math.pow(parseInt(inEvent.clientY-initY),2))),0.5);
 
-     //To draw equilateral triangle, we have initial point and let us asssume the obtained distance as the hegiht of tringle.
-     //Using this, we can get the side of the tringle, thereby we obtain the right and left side vertices points.
-
-    triSide=1.1547*dist;
-            
-	ctx.beginPath();
-	ctx.moveTo(initX, initY);
-	ctx.lineTo(initX-triSide/2,initY+dist);
-	ctx.lineTo(initX+triSide/2,initY+dist);
-	ctx.closePath();
-
-	ctx.lineWidth = 1;
-	ctx.strokeStyle = '#666666';
-	ctx.stroke();
-
-	ctx.fillStyle =getColor();
-	ctx.fill();
-
-}
 
 function getColor()
 {
 	return '#'+Math.random().toString(16).slice(-6);
 }
-
+function pointInTriangle(x1, y1, x2, y2, x3, y3, x, y)
+{
+ var denominator = ((y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3));
+ var a = ((y2 - y3)*(x - x3) + (x3 - x2)*(y - y3)) / denominator;
+ var b = ((y3 - y1)*(x - x3) + (x1 - x3)*(y - y3)) / denominator;
+ var c = 1 - a - b;
+ 
+ return 0 <= a && a <= 1 && 0 <= b && b <= 1 && 0 <= c && c <= 1;
+}
 
     var flag = 0;
     var initX,initY;
@@ -84,6 +85,7 @@ function getColor()
     var triSide;
 
     var triangles = [];
+    var triSel=false;
 
 
 // To get the initial point 
@@ -95,16 +97,53 @@ document.addEventListener("mousedown", function(event){
 
 // To enable click into drag event 
 document.addEventListener("mousemove", function(){
+    if(!triSel)
     flag = 1;
+else
+{
+    console.log('trianvle is dragged');
+    console.log(event);
+}
 }, false);
 
 // To draw triandle according to drag size
-
-document.addEventListener("mouseup", drawTriangle, false);
 
 document.addEventListener("mouseup", function(){
    if(flag === 1){
         drawTriangle(event);
     }
+    else
+    {
+      triSel=false;  
+    }
+}, false);
+canvas.addEventListener("mousemove", function(event){
+    
+   for(var i=0;i<triangles.length;i++)
+   {
+    var x1=triangles[i].x1;
+    var y1=triangles[i].y1;
+    var x2=triangles[i].x2;
+    var y2=triangles[i].y2;
+    var x3=triangles[i].x3;
+    var y3=triangles[i].y3;
+    var x=event.clientX;
+    var y=event.clientY;
+    var check_point=pointInTriangle(x1, y1, x2, y2, x3, y3, x, y);
+    if(check_point)
+    {
+        if(flag==0)
+        {
+        console.log('inside tri');
+        triSel=true;
+        var triSelected=triangles[i];
+
+    }
+    }
+   }
 }, false);
 
+canvas.addEventListener("drag", function(event){
+if(triSel)
+ console.log('dragging');
+},false);
